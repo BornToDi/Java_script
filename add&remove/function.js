4ebe983dc2bd7a22872d2dcc7766946d38d5fd6d
Md. Zihadul islam <98985851+BornToDi@users.noreply.github.com>
2023-02-07 17:16:16 +0600

    Add files via upload

---
+++ add&remove/function.js
@@ -0,0 +1,10 @@
+
+
+function addStyle(){
+    var x = document.querySelector("#colorId");
+    x.classList.add("spot");
+}
+function removeStyle(){
+    var x = document.querySelector("#colorId");
+    x.classList.remove("spot");
+}
