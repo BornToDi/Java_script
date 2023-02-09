01ae2077c1cbf259b471efe8d1014c65f217dd7f
Md. Zihadul islam <98985851+BornToDi@users.noreply.github.com>
2023-02-09 17:10:34 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-//var myVar= document.querySelector("#myId");
 
-function myMessege1(){
 
-    var myVar= document.querySelector("#paraId");
-    myVar.innerHTML="I am a person who is positive about every aspect of life";
 
-}
+var buttons = document.querySelectorAll(".submit");
+console.log("buttons");
+Array.from(buttons).map((button) => {
 
-function myMessege2(){
-    var myVar= document.querySelector("#paraId");
-    myVar.innerHTML= " I love to play cricket";
-}
+    button.addEventListener("click", function(e){
+        console.log(e.target.innerText);
+    });
+});
